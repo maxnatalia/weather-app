1f3db8f0e5c27e43262185ca78dd2ee83f5567f7
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-07 09:36:05 +0200

    updated apiData - deleted apiKey

--- src/apiData.js
+++ src/apiData.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-export const apiKey = "6a480d56f34f013ad780d32e2672b0b5";
-
 export const weatherApi = axios.create({
     baseURL: "https://api.openweathermap.org/data/2.5/weather",
     headers: {}
