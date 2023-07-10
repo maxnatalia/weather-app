1961c2e0339260fb0d6f9463bb35dbcad99dec05
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-10 11:49:28 +0200

    created Error component

---
+++ src/features/Error.jsx
@@ -0,0 +1,11 @@
+const Error = () => {
+  return (
+    <div className="bg-gray-600/50 py-4 text-center font-semibold text-emerald-200">
+      <h2 className=" text-3xl ">Error</h2>
+      <p className="text-2xl">Something went wrong...</p>
+      <p>Try again later</p>
+    </div>
+  );
+};
+
+export default Error;
