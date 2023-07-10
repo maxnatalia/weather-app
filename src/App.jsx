cf3956fdceb5876f80a7f3654fd8459d42e9e4ef
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-10 11:50:36 +0200

    created App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-function App() {
+import { useFetchWeather } from "./useFetchWeather";
+import Header from "./features/Header";
+import Main from "./features/Main";
+import Loading from "./features/Loading";
+import Error from "./features/Error";
+import Weather from "./features/Weather";
+import Footer from "./features/Footer";
+import useNewCity from "./useNewCity";
+
+const App = () => {
+  const { newCity, city, onFormSubmit, handleCityChange } = useNewCity();
+  const { data, isFetching, isError } = useFetchWeather(newCity);
+
   return (
-    <>
-      <div>Hello!</div>
-    </>
+    <div className="mx-auto flex min-h-screen  max-w-screen-2xl flex-col bg-main">
+      <Header
+        city={city}
+        handleCityChange={handleCityChange}
+        onFormSubmit={onFormSubmit}
+      />
+      <Main>
+        {isFetching && <Loading />}
+        {isError && <Error />}
+        {data && <Weather data={data} />}
+      </Main>
+      <Footer />
+    </div>
   );
-}
+};
 
 export default App;
