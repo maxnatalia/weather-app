8aeb2fb5e6e8255b6f97486ff9ac0bb9eef044da
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-10 11:49:14 +0200

    created Loading component

---
+++ src/features/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <h2 className=" bg-gray-600/50 py-4 text-center text-3xl font-semibold text-emerald-200">
+      Loading data...
+    </h2>
+  );
+};
+
+export default Loading;
